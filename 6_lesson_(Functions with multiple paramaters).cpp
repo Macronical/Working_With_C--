e4c01bcba73e3b45595bf4f2b8f41cde8f574851
Macronical <Sebe.l.j@live.no>
2016-10-04 12:59:32 +0200

    Create 6_lesson_(Functions with multiple paramaters).cpp

---
+++ 6_lesson_(Functions with multiple paramaters).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int addNumbers(int x, int y){
+    int answer = x + y;
+
+    return answer;
+}
+
+int main()
+{
+    cout << addNumbers(43, 86);
+
+    return 0;
+}
