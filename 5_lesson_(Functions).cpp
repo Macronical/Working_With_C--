950cc22390181f2dee433fe98e74ef5dddf6f8be
Macronical <Sebe.l.j@live.no>
2016-10-04 12:44:42 +0200

    Create 5_lesson_(Functions).cpp

---
+++ 5_lesson_(Functions).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void printCrap(int x){
+    cout << "Your favorite number is: " << x << endl;
+}
+
+int main()
+{
+    printCrap(15);
+
+    return 0;
+}
